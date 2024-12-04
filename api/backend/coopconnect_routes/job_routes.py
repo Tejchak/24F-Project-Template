581b4d999a02ac61c8ea2e1fcfe7b563ec90276e
dkimura25 <kimura.d@northeastern.edu>
2024-12-03 21:15:00 -0500

    Update job_routes.py

--- api/backend/coopconnect_routes/job_routes.py
+++ api/backend/coopconnect_routes/job_routes.py
@@ -17,6 +17,21 @@ def get_all_jobs():
     the_response.status_code = 200
     return the_response
 
+@performance.route('/jobs/user/<UserID>', methods=['GET'])
+def get_jobs_by_user(UserID):
+    cursor = db.get_db().cursor()
+    query = """
+    SELECT Job_ID, Wage, Employment_Status, start_date
+    FROM Job
+    WHERE User_ID = %s
+    """
+    cursor.execute(query, (UserID,))
+    theData = cursor.fetchall()
+
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
+
 @performance.route('/job_postings/location/<Location_ID>', methods=['GET'])
 def get_job_postings_by_location(Location_ID):
     cursor = db.get_db().cursor()
