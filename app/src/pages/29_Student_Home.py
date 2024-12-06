20bde472ffa6aa5ae2d9e326cc18821076cbc20a
dkimura25 <kimura.d@northeastern.edu>
2024-12-06 10:50:33 -0500

    Update 29_Student_Home.py

--- app/src/pages/29_Student_Home.py
+++ app/src/pages/29_Student_Home.py
@@ -1,57 +1,41 @@
 import logging
+logger = logging.getLogger(__name__)
+
 import streamlit as st
 from modules.nav import SideBarLinks
-
-logger = logging.getLogger(__name__)
 
 st.set_page_config(layout='wide')
 
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
-# Welcome message for the student
+# Welcome the student
 if 'first_name' in st.session_state:
     st.title(f"Welcome Student, {st.session_state['first_name']}.")
 else:
     st.title("Welcome Student.")
+
 st.write('')
 st.write('')
 st.write('### What would you like to do today?')
 
-# Navigation options for student persona
+# Navigation buttons for student actions
 if st.button('Browse Available Jobs', 
              type='primary',
              use_container_width=True):
     st.switch_page('pages/37_Browse_Jobs.py')
-
-if st.button('Student Map', 
-            type = 'primary', 
-            use_container_width=True):
-    st.switch_page('pages/34_student_map.py')
 
 if st.button('Apply for a Job', 
              type='primary',
              use_container_width=True):
     st.switch_page('pages/38_Apply_Job.py')
 
-if st.button('View Application Status',
-             type='primary',
-             use_container_width=True):
-    st.switch_page('pages/39_View_Applications.py')
-
-if st.button('Get Personalized Recommendations',
-             type='primary',
-             use_container_width=True):
-    st.switch_page('pages/43_Job_Recs.py')
-
-
-if st.button('Update Your Profile',
+if st.button("Update Your Profile",
              type='primary',
              use_container_width=True):
     st.switch_page('pages/44_Update_Profile.py')
-    
 
-if st.button('Delete an Application',
+if st.button("View and Manage Sublets",
              type='primary',
              use_container_width=True):
-    st.switch_page('pages/45_Delete_App.py')
+    st.switch_page('pages/45_Manage_Sublets.py')
